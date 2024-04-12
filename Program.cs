03b9c6237cb9cb15bb1fefb2b3b059bc15707760
DanielGorin <daniel.l.gorin@gmail.com>
2024-04-12 16:13:36 +0200

    FInalized the Scale method and updated the Reset Method SUch that boht methods alos search the instructions to scale them

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
 using System.Text;
@@ -34,10 +35,10 @@ namespace PROG2APOEQ1
             {
                 Console.WriteLine("What is the name of ingredient " + (i+1));
                 IngredientName[i] = Console.ReadLine();
-                Console.WriteLine("How much (a number) of ingredient " + (i + 1)+" is used");
-                IngredientQuant[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("How much (a number) of ingredient " + (i + 1)+" is used (use a full stop i decimals Eg 5.55)");
+                IngredientQuant[i] = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
                 OrigIngredientQuant[i] = IngredientQuant[i];
-                Console.WriteLine("What is the measurment used for ingredient " + (i + 1));
+                Console.WriteLine("What is the unit of measurment used for ingredient " + (i + 1));
                 Ingredientmeasure[i] = Console.ReadLine(); 
             }
             Console.WriteLine("how many steps are there in your recipe?");
@@ -45,7 +46,7 @@ namespace PROG2APOEQ1
             StepDescriptions = new string[NumberSteps];
             for (int j = 0;  j < NumberSteps; j++)
             {
-                Console.WriteLine("what is the instruction for step " + (j+1));
+                Console.WriteLine("what is the instruction for step " + (j+1)+ " (use a comma in decimals Eg 5,55)");
                 StepDescriptions[j] = Console.ReadLine();
             }
             Console.WriteLine("Recipe titled " + RecipeName + " succesfully created.");
@@ -74,11 +75,52 @@ namespace PROG2APOEQ1
         }
         // End of DisplayRecipe Method
 
+        //ScaleQuant method
+        //sclaes the quantity of the ingredients by the desired factor
+        public void ScaleQuant(float scale)
+        {
+            for (int i = 0; i < NumberIngredients; i++)
+            {
+                
+                //seekingspace and settingspace are for units such as cups or teaspoons Eg 6 cups of suger
+                string seekingspace = ((IngredientQuant[i]).ToString() +" " + Ingredientmeasure[i] +" of " + IngredientName[i]);
+                string settingspace = ((IngredientQuant[i]*scale).ToString() + " " + Ingredientmeasure[i] + " of " + IngredientName[i]);
+                //seekingnospace and settingnospace is for units such as kg or ml Eg 6Kg of cheese
+                string seekingnospace = ((IngredientQuant[i]).ToString() + ""+ Ingredientmeasure[i] + " of " + IngredientName[i]);
+                string settingnospace = ((IngredientQuant[i]*scale).ToString() + "" + Ingredientmeasure[i] + " of " + IngredientName[i]);
+
+                for (int j = 0; j < NumberSteps; j++)
+                {
+                    StepDescriptions[j]=StepDescriptions[j].Replace(seekingspace, settingspace);
+                    StepDescriptions[j]=StepDescriptions[j].Replace(seekingnospace, settingnospace);
+                }
+                IngredientQuant[i] *= scale;
+            }
+            Console.WriteLine("Ingredient quantities scaled succesfully");
+        }
+        //end of the ScaleQuant method
+
         //ResetQuant method
         //Sets the QUantity values to the inital ones input (relevant after the use of the scale QUant method)
         public void ResetQuant()
         {
+            for (int i = 0; i < NumberIngredients; i++)
+            {
+                //seekingspace and settingspace are for units such as cups or teaspoons Eg 6 cups of suger
+                string seekingspace = ((IngredientQuant[i]).ToString() + " " + Ingredientmeasure[i] + " of " + IngredientName[i]);
+                string settingspace = ((OrigIngredientQuant[i]).ToString() + " " + Ingredientmeasure[i] + " of " + IngredientName[i]);
+                //seekingnospace and settingnospace is for units such as kg or ml Eg 6Kg of cheese
+                string seekingnospace = ((IngredientQuant[i]).ToString() + "" + Ingredientmeasure[i] + " of " + IngredientName[i]);
+                string settingnospace = ((OrigIngredientQuant[i]).ToString() + "" + Ingredientmeasure[i] + " of " + IngredientName[i]);
+
+                for (int j = 0; j < NumberSteps; j++)
+                {
+                    StepDescriptions[j]=StepDescriptions[j].Replace(seekingspace, settingspace);
+                    StepDescriptions[j]=StepDescriptions[j].Replace(seekingnospace, settingnospace);
+                }
+            }
             IngredientQuant = OrigIngredientQuant;
+            Console.WriteLine("Quantities have been reset");
         }
         //End of ResetQuant method
         
@@ -113,6 +155,24 @@ namespace PROG2APOEQ1
                 }else if(inpt == "Scale Quant")
                 {
                     //The Scale Quant funciton prompts the user as to how much they want to scale the quantity of the ingredient and then calls up the scale function
+                    
+                    Console.WriteLine("What factor would you like the quantity of ingredients to be scaled by (enter Half, Double or Triple)");
+                    string txtscl = Console.ReadLine();
+                    if ((txtscl == "Half"))
+                    {
+                        recipe.ScaleQuant(0.5F);
+                    }
+                    else if (txtscl == "Double")
+                    {
+                        recipe.ScaleQuant(2);
+                    }
+                    else if (txtscl == "Triple")
+                    {
+                        recipe.ScaleQuant(3);
+                    }else
+                    {
+                        Console.WriteLine("That is not a valid scaling factor");
+                    }
 
                 }else if( inpt =="Reset Quant")
                 {
