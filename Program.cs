f5aca3cfacabb2f984aac4aabacd101338538bbe
DanielGorin <daniel.l.gorin@gmail.com>
2024-04-12 16:31:06 +0200

    Neatend code and finalized coments

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using System.Globalization;
 using System.Linq;
 using System.Runtime.Remoting.Channels;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PROG2APOEQ1
 {
@@ -50,11 +49,9 @@ namespace PROG2APOEQ1
                 StepDescriptions[j] = Console.ReadLine();
             }
             Console.WriteLine("Recipe titled " + RecipeName + " succesfully created.");
-
         }
         //---------------------------- End of Recipe Constructors -------------------------
         //---------------------------- Recipe Mehthods -------------------------------------
-
         //DisplayRecipe Method
         //Will print the full recipe in a pleasent format
         public void DisplayRecipe() 
@@ -70,25 +67,21 @@ namespace PROG2APOEQ1
             for (int j = 0;j < NumberSteps; j++)
             {
                 Console.WriteLine((j+1)+". " + StepDescriptions[j]);
-            }
-            
+            } 
         }
         // End of DisplayRecipe Method
-
         //ScaleQuant method
         //sclaes the quantity of the ingredients by the desired factor
         public void ScaleQuant(float scale)
         {
             for (int i = 0; i < NumberIngredients; i++)
             {
-                
                 //seekingspace and settingspace are for units such as cups or teaspoons Eg 6 cups of suger
                 string seekingspace = ((IngredientQuant[i]).ToString() +" " + Ingredientmeasure[i] +" of " + IngredientName[i]);
                 string settingspace = ((IngredientQuant[i]*scale).ToString() + " " + Ingredientmeasure[i] + " of " + IngredientName[i]);
                 //seekingnospace and settingnospace is for units such as kg or ml Eg 6Kg of cheese
                 string seekingnospace = ((IngredientQuant[i]).ToString() + ""+ Ingredientmeasure[i] + " of " + IngredientName[i]);
                 string settingnospace = ((IngredientQuant[i]*scale).ToString() + "" + Ingredientmeasure[i] + " of " + IngredientName[i]);
-
                 for (int j = 0; j < NumberSteps; j++)
                 {
                     StepDescriptions[j]=StepDescriptions[j].Replace(seekingspace, settingspace);
@@ -99,7 +92,6 @@ namespace PROG2APOEQ1
             Console.WriteLine("Ingredient quantities scaled succesfully");
         }
         //end of the ScaleQuant method
-
         //ResetQuant method
         //Sets the QUantity values to the inital ones input (relevant after the use of the scale QUant method)
         public void ResetQuant()
@@ -123,21 +115,16 @@ namespace PROG2APOEQ1
             Console.WriteLine("Quantities have been reset");
         }
         //End of ResetQuant method
-        
-
         //---------------------------- End of Recipe Methods ------------------------------
-
-
-
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            String inpt=" "; //the input string will hold the text instructions the user inputs
+            String inpt=""; //the input string will hold the text instructions the user inputs
             Recipe recipe = new Recipe();
             recipe.CreateRecipe();
-            while (inpt != "Exit")
+            while (inpt != "Exit")// This will loop until the user types the Exit Command at which point the program will end
             {
                 Console.WriteLine();
                 Console.WriteLine("Please use any of the following comands:");
@@ -151,16 +138,13 @@ namespace PROG2APOEQ1
                     Console.WriteLine("Rese Quant -> will reset the ingredeint quantities and the instructions back to their original values");
                     Console.WriteLine("Cear -> Will wipe your recipe and prompt you to create a new one");
                     Console.WriteLine("Exit -> Will Shutdown the program closing the comand line interface");
-
                 }else if (inpt == "Display")
                 {
                     //The display command calls up the dislay funciton
                     recipe.DisplayRecipe();
-
                 }else if(inpt == "Scale Quant")
                 {
                     //The Scale Quant funciton prompts the user as to how much they want to scale the quantity of the ingredient and then calls up the scale function
-                    
                     Console.WriteLine("What factor would you like the quantity of ingredients to be scaled by (enter Half, Double or Triple)");
                     string txtscl = Console.ReadLine();
                     if ((txtscl == "Half"))
@@ -178,7 +162,6 @@ namespace PROG2APOEQ1
                     {
                         Console.WriteLine("That is not a valid scaling factor");
                     }
-
                 }else if( inpt =="Reset Quant")
                 {
                     //The Reset Quant Comand calls up the rest funciton
